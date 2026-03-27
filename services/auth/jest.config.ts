bc0c26b603ef9db0b06128dd0e36279558be9840
suyan thapa <suyanthapa07@gmail.com>
2026-03-27 10:42:47 +0545

    chore(config): remove tsconfig.test.json, consolidate into tsconfig.json

--- services/auth/jest.config.ts
+++ services/auth/jest.config.ts
@@ -9,7 +9,7 @@ const config: JestConfigWithTsJest = {
     "^.+\\.ts$": [
       "ts-jest",
       {
-        tsconfig: "tsconfig.test.json",
+        tsconfig: "tsconfig.json",
       },
     ],
   },
